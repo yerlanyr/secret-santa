6b54f4b8fc6cd816a60b90236245fe1d172c941b
Yerlan <yerlanyr@gmail.com>
2020-01-11 18:43:07 +0600

    Refactor test to better encompass the BDD approach.

--- test/server.test.js
+++ test/server.test.js
@@ -1,19 +1,12 @@
-const {expect} = require('chai');
-describe('server', () => {
-    it('It should delete key when delete called', function(){
-        const userNames = {
-            'id': 'name'
-        };
-        expect(userNames['id']).to.equal('name');
-        delete userNames['id'];
-        expect(userNames['id']).to.be.undefined;
-    });
-    it('Test distribution of indexes', () => {
+const { expect } = require('chai');
+const { generate } = require('../server/indexes-generator');
+
+describe('Index generator', () => {
+    it('should perform shuffle algorithm on 0 to N generated array of integers.', () => {
         const n = 10;
-        const arr = require('../server/indexes-generator').generate(n);
+        const arr = generate(n);
         console.log(arr);
         expect(arr.length).to.equal(10);
-        // all distinct
         expect(Array.from(new Set(arr)).length).to.equal(10);
         expect(Math.max(...arr)).to.equal(9);
         expect(Math.min(...arr)).to.equal(0);
